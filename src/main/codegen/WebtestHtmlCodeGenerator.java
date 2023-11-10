a782676f801eab87e5fe5fd273ab511f995a3ac2
Fsomogyi <ferencsomogyi92@gmail.com>
2023-11-10 12:53:42 +0100

    hotfix path

--- src/main/codegen/WebtestHtmlCodeGenerator.java
+++ src/main/codegen/WebtestHtmlCodeGenerator.java
@@ -2,9 +2,11 @@ package main.codegen;
 
 import main.data.WebtestInputTable;
 
+import java.nio.file.Paths;
+
 // TODO: HTML code generation
 public class WebtestHtmlCodeGenerator extends WebtestCodeGenerator {
     public WebtestHtmlCodeGenerator(WebtestInputTable inputTable) {
-        super("src\\main\\codegen\\stringtemplate\\HtmlGen.stg", "html", inputTable);
+        super(Paths.get("src", "main", "codegen", "stringtemplate", "HtmlGen.stg").toString(), "html", inputTable);
     }
 }
